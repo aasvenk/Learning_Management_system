8b8a2cac7cdbd619c97df93e7596485a158cbcd4
Aashish Venkatesh Gautham <aasvenk@iu.edu>
2023-09-21 12:17:13 -0400

    Login page with styling and button feature

--- frontend/hoosier-room/src/App.js
+++ frontend/hoosier-room/src/App.js
@@ -1,27 +1,80 @@
+import React, { useState } from 'react';
+import './App.css'; 
 
-function Login() {
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Client-side validation
+    if (username.trim() === '') {
+      setErrorMessage('Username cannot be empty');
+      return;
+    }
+
+    // Password validation
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+    if (!password.match(passwordRegex)) {
+      setErrorMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special symbol');
+      return;
+    }
+
+    // need to add logic if both password and username is correct 
+  };
+
+  const handleForgotPassword = () => {
+    // Need to add logic for Forgot password page redirect
+
+  };
+
+  const handleSignUp = () => {
+    // Need to add logic for sign up page redirect
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1> Login Page</h1> 
-        <form> 
-          <div id = "Login">  
-            <label> 
-              Username: <br></br>
-              <input type = "text" id ="username" Placeholder = "Enter your Username"/>
-              </label> 
-            </div>
-            <label>
-            Password: 
-            </label>
-            <br></br>
-            <input type = 'password' id = "password" Placeholder ="Enter your Password"/>
-            <label> <br></br></label>
-            <input type ='Submit' id = "Submit" value = "Submit"  />
-        </form>
-      </header>
+    <div className="login-page">
+      <h1> Hoosier Room</h1>
+      <h2>Login</h2>
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
+      <div className="login-form">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button onClick={handleLogin}>Login</button>
+        <div className="additional-options">
+          <button onClick={handleForgotPassword}>Forgot Password</button>
+          <button onClick={handleSignUp}>Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Login;
+export default LoginPage;
