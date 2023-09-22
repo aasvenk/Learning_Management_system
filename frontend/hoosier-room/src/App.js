bf687e3191c2c9c84084ad7f238c7b494cde777a
Nikhil Vemula <srvemu@iu.edu>
2023-09-22 11:57:24 -0400

    Add more styling

--- frontend/hoosier-room/src/App.js
+++ frontend/hoosier-room/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [usernameError, setUsernameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -15,16 +16,29 @@ function LoginPage() {
   };
 
   const handleLogin = () => {
+
+    setUsernameError('')
+    setPasswordError('')
+
+    let isFormValid = true
     // Client-side validation
     if (username.trim() === '') {
-      setErrorMessage('Username cannot be empty');
-      return;
+      setUsernameError('Username is required');
+      isFormValid = false
     }
 
     // Password validation
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
-    if (!password.match(passwordRegex)) {
-      setErrorMessage('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special symbol');
+    // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+    // if (!password.match(passwordRegex)) {
+    //   setPasswordError('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special symbol');
+    //   return;
+    // }
+    if (password.trim() === '') {
+      setPasswordError('Password is required');
+      isFormValid = false
+    }
+
+    if (!isFormValid) {
       return;
     }
 
@@ -44,7 +58,6 @@ function LoginPage() {
   return (
     <div className="login-page">
       <h1> Hoosier Room</h1>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
       <div className="login-form">
         <h2>Login in to your acccount</h2>
         <div className="form-group">
@@ -57,6 +70,7 @@ function LoginPage() {
             value={username}
             onChange={handleUsernameChange}
           />
+          {usernameError && <div className="error-message">{usernameError}</div>}
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
@@ -68,6 +82,7 @@ function LoginPage() {
             value={password}
             onChange={handlePasswordChange}
           />
+          {passwordError && <div className="error-message">{passwordError}</div>}
         </div>
         <div class="actions">
           <button onClick={handleLogin}>Login</button>
