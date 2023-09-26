5c92861dda4b4e31b363cf04bd46e2c442fba9da
Aashish Venkatesh Gautham <aasvenk@iu.edu>
2023-09-26 15:14:36 -0400

    Signup page with styling and client side validations

--- frontend/src/Pages/SignupPage.js
+++ frontend/src/Pages/SignupPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import  "./SignUpPage.css";
 
 function SignupPage() {
   const [firstName, setFirstName] = useState("");
@@ -8,6 +9,7 @@ function SignupPage() {
   const [email,setEmail] = useState("");
   const [securityQuestion, setSecurityQuestion] = useState("");
   const [error, setError] = useState("");
+
 
   const handleSignup = () => {
     setError(""); 
@@ -25,6 +27,14 @@ function SignupPage() {
       return;
     }
 
+    //check if password contains required characters and special symbols 
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+    if (!password.match(passwordRegex)) {
+      setError('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special symbol');
+      return;
+    }
+
     // Check if passwords match
     if (password !== passwordRetype) {
       setError("Passwords do not match.");
@@ -35,65 +45,84 @@ function SignupPage() {
 
   return (
     <div>
-      <h1>Sign Up</h1>
-      {error && <div className="error-message">{error}</div>}
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
+      <div className="Signup-header">
+        <span className="title">Hoosier Room</span>
       </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          id="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
+      <div className = 'Sign-Up'>
+        <div className="signup-form">
+          <h1>Sign Up</h1>
+          {error && <div className="error-message">{error}</div>}
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <span className="required">*</span>
+            <input
+              type="text"
+              id="firstName"
+              placeholder= "Enter Your First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <span className="required">*</span>
+            <input
+              type="text"
+              id="lastName"
+              placeholder= "Enter Your Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <span className="required">*</span>
+            <input
+              type="password"
+              id="password"
+              placeholder= "Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="passwordRetype">Confirm your Password:</label>
+            <span className="required">*</span>
+            <input
+              type="password"
+              id="passwordRetype"
+              placeholder= "Retype your Password"
+              value={passwordRetype}
+              onChange={(e) => setPasswordRetype(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <span className="required">*</span>
+            <input
+              type="email"
+              id="email"
+              placeholder= "Enter Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="securityQuestion">Security Question: What is your birth City?</label>
+            <span className="required">*</span>
+            <input
+              type="text"
+              id="securityQuestion"
+              placeholder= "Type your Birth Place"
+              value={securityQuestion}
+              onChange={(e) => setSecurityQuestion(e.target.value)}
+            />
+          </div>
+          <button onClick={handleSignup}>Sign Up</button>
+        </div>
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="passwordRetype">Password (Retype)</label>
-        <input
-          type="password"
-          id="passwordRetype"
-          value={passwordRetype}
-          onChange={(e) => setPasswordRetype(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="securityQuestion">Security Question: What is your birth City?</label>
-        <input
-          type="text"
-          id="securityQuestion"
-          value={securityQuestion}
-          onChange={(e) => setSecurityQuestion(e.target.value)}
-        />
-      </div>
-      <button onClick={handleSignup}>Sign Up</button>
     </div>
-  );
+    );
 }
 
 export default SignupPage;
