e0234ff78a1050e5a5dabfa5ff5b8938c5e6134b
Nikhil Vemula <srvemu@iu.edu>
2023-09-26 16:02:44 -0400

    Add ui for recover password ui

---
+++ frontend/src/Pages/ForgotPasswordPage/ForgotPasswordPage.js
@@ -0,0 +1,15 @@
+import AppHeader from "../../components/AppHeader";
+import RecoverOptions from "../../components/RecoverOptions";
+import "./ForgotPassword.css";
+
+
+function ForgotPassword() {
+  return (
+    <div>
+      <AppHeader />
+      <RecoverOptions />
+    </div>
+  );
+}
+
+export default ForgotPassword;
--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+
 function AppHeader() {
   return (
-    <div>Hello</div>
+    <div className="login-header">
+      <Link to="/">
+        <span className="title">Hoosier Room</span>
+      </Link>
+      <p>
+        <span className="signup-msg">Don't have an account?</span>
+        <Link to="/signup" className="signup-link">
+          Sign up
+        </Link>
+      </p>
+    </div>
   );
 }
 
