0fd1b06c28437ae3b600bdd585b0b08db5b14a9b
Venkatesh Gautham, Aashish <aasvenk@iu.edu>
2023-09-28 01:16:00 -0400

    Reset Password UI (#13)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 import ForgotPassword from "./Pages/ForgotPasswordPage/ForgotPasswordPage";
+import ChangePassword from  "./Pages/ChangePassword"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path = "/change-password" element = {<ChangePassword />} />
       </Routes>
     </Router>
   );
