26a7007d13aa12f6f3fe041a5eb6baeaa8ddbe14
Venkatesh Gautham, Aashish <aasvenk@iu.edu>
2023-10-16 18:43:12 -0400

    Added Course page details with announcement UI (#24)

--- frontend/src/Pages/CoursePage/CoursePage.js
+++ frontend/src/Pages/CoursePage/CoursePage.js
@@ -1,17 +1,89 @@
 import { useParams } from "react-router-dom";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
 import AppHeader from "../../components/AppHeader";
+import  "./CoursePage.css";
+
 
 function CoursePage() {
-  const { id } = useParams()
+  const { courseNumber } = useParams()
+
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
+
+
+
+  // Get details of course based on id (api-call)
+  // {title: " ", desc: ""}
+  
+  const course ={ 
+    
+    title : 'Course 1',
+    Description: "Descirption of course 1",  
+  };
+
+  const Announcements = [
+    {
+      "id" : "1",
+      "title" : "Announcement 1",
+      "Description" : " Desctiption of Announcement 1"
+    },
+    {
+      "id" : "2",
+      "title" : "Announcement 2",
+      "Description" : " Desctiption of Announcement 2"
+    }
+  ];
 
   return (
     <div>
-      <AppHeader />
-      <div className="page-container">
-        {id}
+      <div> 
+        <AppHeader />
+      </div>
+      <div className="course-page">
+      <Paper elevation={2} style={{ padding: "10px" }}>
+        <Box sx={{ width: "100%", typography: "body1" }}>
+          <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Course Description" value="1" />
+                <Tab label = "Announcements" value = "2" /> 
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              <div>
+                <h1>{course.title} </h1>
+              </div>
+              <div>
+              {course.Description} </div></TabPanel>
+          
+            <TabPanel value="2">
+              <div>
+                  {Announcements.map((announcement, i) => { 
+                    return (
+                      <div key={i} className="Announcement">
+                        <h1>{announcement.title} </h1>
+                        <div>{announcement.Description}</div>
+                      </div>
+                    );
+                  })}
+               </div></TabPanel>
+          </TabContext>
+        </Box>
+      </Paper>
       </div>
     </div>
   );
-}
-
+}   
 export default CoursePage;
