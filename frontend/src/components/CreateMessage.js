5a7acab082ff275b4b2f358a1f6488d1e4da01e0
Nicholas Van Burk <ndvabur@iu.edu>
2023-11-12 11:36:52 -0500

    setting up branch for create message

---
+++ frontend/src/components/CreateMessage.js
@@ -0,0 +1,6 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
